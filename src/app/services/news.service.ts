2cbd679be14f7926e4495c27972b1fccf952e64d
Peter G. Horvath <peter.gergely.horvath@gmail.com>
2019-11-04 20:23:23 +0100

    Changed the location of the news feed data file

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -29,12 +29,12 @@ export class NewsService {
     }
 
     initArticles() {
-        return this.http.get('http://data.cloudoverload.com/aws/internet-news.json')
+        return this.http.get('http://data.cloudoverload.com/aws/news/news.json')
             .pipe(map(data => data['entries']))
     }
 
     getArticlesByVendorCode(vendor: String){
-           return this.http.get('http://data.cloudoverload.com/'+vendor+'/internet-news.json')
+           return this.http.get('http://data.cloudoverload.com/'+vendor+'/news/news.json')
             .pipe(map(data => data['entries']))
     }
 }
